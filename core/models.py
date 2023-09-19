d332317e7d7b99c77965c70ac48c809a1b3e833a
NiramayThaker <thakerniramay@gmail.com>
2023-09-19 12:44:07 +0530

    Model creation and schema designing

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BookAssigned(models.Model):
+	username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	# book_name = models.CharField(max_length=25)
+
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
--- core/views.py
+++ core/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 
 # Create your views here.
+@login_required(login_url='login/')
 def home(request):
 	return render(request, 'core/index.html')
 
 
 def sign_up(request):
 	form = UserRegistrationForm()
+	if request.method == "POST":
+		form = UserRegistrationForm(request.POST)
+
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect("home")
 
 	context = {'form': form}
 	return render(request, 'registration/signup.html', context=context)
