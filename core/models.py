a8244b2269eec655df5cd981d6d5e39901046768
NiramayThaker <thakerniramay@gmail.com>
2023-09-19 20:12:32 +0530

    Model creation and schema designing

--- core/models.py
+++ core/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class BookDetail(models.Model):
 	# username = models.ForeignKey(User, on_delete=models.CASCADE)
-	book_name = models.CharField(max_length=25)
-	author = models.CharField(max_length=25)
+	book_name = models.CharField(max_length=50)
+	author = models.CharField(max_length=50)
 	price = models.IntegerField()
 	available = models.BooleanField(default=True)
-	tag1 = models.CharField(max_length=25)
-	tag2 = models.CharField(max_length=25)
+	tag1 = models.CharField(max_length=50)
+	tag2 = models.CharField(max_length=50)
 
 	def __str__(self):
 		return f"{self.book_name} by {self.author}"
@@ -19,8 +19,8 @@ class BookDetail(models.Model):
 class BookAssigned(models.Model):
 	username = models.ForeignKey(User, on_delete=models.CASCADE)
 	book_name = models.ForeignKey(BookDetail, on_delete=models.CASCADE)
-	created_at = models.DateTimeField(auto_now_add = True)
-	updated_at = models.DateTimeField(auto_now = True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return f"{self.username} has {self.book_name}"
